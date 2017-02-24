2a8ba0eab064d0d1c263efec92869926b3ac601f
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2017-02-24 23:36:16 +0200

    Global state machine implemented. Message box partially implemented

--- Src/GPSLogger.ino
+++ Src/GPSLogger.ino
@@ -21,10 +21,42 @@ Adafruit_SSD1306 display;
 const int SEL_BUTTON_PIN = 2;
 const int OK_BUTTON_PIN = 3;
 
+const unsigned long DEBOUNCE_TIMEOUT = 30;
+const unsigned long MESSAGE_BOX_DIRATION = 5000;
 
-//SoftwareSerial gpsSerial(10, 11); // RX, TX
+enum State
+{
+	IDLE_DISPLAY_OFF,
+	IDLE,
+	MESSAGE_BOX,
+	BUTTON_PRESSED,
+};
+
+State state = IDLE; // Current state
+unsigned long lastEventMillis = 0; // Milis() for last state change
+
+enum ButtonsEnum
+{
+	NO_BUTTON,
+	SEL_BUTTON,
+	OK_BUTTON
+};
+
+ButtonsEnum curPressedButton = NO_BUTTON;
+
+int8_t getCurPressedButton()
+{
+	if(!digitalRead(SEL_BUTTON_PIN))
+		return SEL_BUTTON;
+
+	if(!digitalRead(OK_BUTTON_PIN))
+		return OK_BUTTON;
+		
+	return NO_BUTTON;
+}
 
 TinyGPSPlus gps;
+//SoftwareSerial gpsSerial(10, 11); // RX, TX
 
 // the setup function runs once when you press reset or power the board
 void setup()
@@ -39,88 +71,117 @@ void setup()
 	pinMode(OK_BUTTON_PIN, INPUT_PULLUP);
 	
 //	gpsSerial.begin(9600);
-
-/*	
-	TestScreen * childScreen = new TestScreen(F("Parent Screen 1"));
-	Screen * child = childScreen->addChildScreen(new TestScreen(F("Child Screen 1")));
-	child = child->addScreen(new TestScreen(F("Child Screen 2")));
-	child = child->addScreen(new TestScreen(F("Child Screen 3")));
-
-	TestScreen * childScreen2 = new TestScreen(F("Child/Parent screen"));
-	child = childScreen2->addChildScreen(new TestScreen(F("Child Screen 4")));
-	child = child->addScreen(new TestScreen(F("Child Screen 5")));
-	child = child->addScreen(new TestScreen(F("Child Screen 6")));
-
-	TestScreen * childScreen3 = new TestScreen(F("Parent Screen 2"));
-	child = childScreen3->addChildScreen(new TestScreen(F("Child Screen 7")));
-	child = child->addScreen(new TestScreen(F("Child Screen 8")));
-	child = child->addScreen(childScreen2);
-	*/
-
 	
 	Screen * screen = createCurrentTimeScreen();
 	setCurrentScreen(screen);
 	screen = screen->addScreen(new CurrentPositionScreen());
-	
-/*	
-	screen = screen->addScreen(new TestScreen(F("No children screen")));
-	screen = screen->addScreen(childScreen);
-	screen = screen->addScreen(childScreen3);
-	screen = screen->addScreen(new TestScreen(F("Another empty Screen")));
-	*/
 }
 
+// Display information according to current state and screen
+void drawDisplay()
+{
+	switch(state)
+	{
+		// Just update display if idle
+		case IDLE:
+		{
+			display.clearDisplay();
+			Screen * currentScreen = getCurrentScreen();
+			currentScreen->drawHeader();
+			currentScreen->drawScreen();
+			display.display();
+			break;
+		}
+		
+		// Draw a message box if requested
+		case MESSAGE_BOX:
+		{
+			display.clearDisplay();
+			display.setFont(NULL);
+			display.drawRect(2, 2, 126, 30, 1);
+			//display.drawRoundRect(2, 2, 126, 30, 4, 1);
+			display.setCursor(10, 12);
+			display.print(F("Message Box!"));
+			display.display();
+			break;
+		}
+
+		// Nothing to do for other states
+		default:
+			break;
+	}
+}
+
+void processStateChanges()
+{
+	switch(state)
+	{
+		// Check if user pressed a button
+		case IDLE:
+			// if so, try to debounce it first
+			if (!curPressedButton && (curPressedButton = getCurPressedButton()))  // Button was not pressed before and now gets pressed
+			{
+				lastEventMillis = millis();
+				state = BUTTON_PRESSED;
+			}
+	
+			break;
+		
+		// Process button release
+		case BUTTON_PRESSED:
+		{
+			ButtonsEnum prevButtonState = curPressedButton;
+			curPressedButton = getCurPressedButton();
+			
+			// Check if button was released after debouncing timeout
+			if (prevButtonState != NO_BUTTON && curPressedButton == NO_BUTTON && (millis() - lastEventMillis > DEBOUNCE_TIMEOUT))
+			{
+				// TODO: Process long press here
+
+				// process button according to current screen
+				Screen * currentScreen = getCurrentScreen();
+				if(prevButtonState == SEL_BUTTON)
+					currentScreen->onSelButton();
+				
+				if(prevButtonState == OK_BUTTON)
+				{
+					//currentScreen->onOkButton();
+					
+					// TODO: Create a function for message boxes
+					state = MESSAGE_BOX;
+					lastEventMillis = millis();
+					break;
+				}
+
+				// Buttons processed - returning to IDLE
+				state = IDLE;
+			}
+
+			break;
+		}
+		
+		// Displaying message box for desired duration
+		case MESSAGE_BOX:
+			if (millis() - lastEventMillis > MESSAGE_BOX_DIRATION)
+				state = IDLE;
+			break;
+		
+		default:
+			break;
+	}
+}
 
 // the loop function runs over and over again forever
 void loop()
 {
-	/*
-	if (gpsSerial.available()) {
-		Serial.write(gpsSerial.read());
-	}
-	if (Serial.available()) {
-		gpsSerial.write(Serial.read());
-	}
-	
-	return;
-	*/
-//	RunFontTest();
+	// TODO: Process GPS data here
 
-	/*
-	while (gpsSerial.available())
-	{
-		uint8_t c = gpsSerial.read(); 		
-		gps.encode(c);
-		Serial.write(c);
-	}
-		
-	Serial.println("Chars:");
-	Serial.println(gps.charsProcessed());
-	Serial.println("sentences:");
-	Serial.println(gps.sentencesWithFix());
-	Serial.println("failed:");
-	Serial.println(gps.failedChecksum());
-	Serial.println("passed:");
-	Serial.println(gps.passedChecksum());
-	*/
-		
-	display.clearDisplay();
-	Screen * currentScreen = getCurrentScreen();
-	currentScreen->drawHeader();
-	currentScreen->drawScreen();
-	display.display();
-	
-	delay(200);
-	
-	if(!digitalRead(SEL_BUTTON_PIN))
-	{
-//		SwitchToNextFont();
-		currentScreen->onSelButton();
-	}
+	// TODO: Sleep here for 50 ms or so
 
-	if(!digitalRead(OK_BUTTON_PIN))
-	{
-		currentScreen->onOkButton();
-	}
+	// Do what we need for current state
+	drawDisplay();
+
+	// It is time for change state
+	processStateChanges();
 }
 
