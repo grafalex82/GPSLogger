92260c55ae28d4322cc98876a703d1d2146b814f
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2017-03-28 23:12:56 +0300

    Display possible odometer action accoring to odometer state

--- Src/OdometerActionScreen.cpp
+++ Src/OdometerActionScreen.cpp
@@ -1,14 +1,7 @@
 #include "GPSOdometer.h"
 #include "OdometerActionScreen.h"
 #include "ScreenManager.h"
-
-const char * actions[] = 
-{
-	"Start",
-	"Resume",
-	"Pause",
-	"Reset"	
-};
+#include "GPSDataModel.h"
 
 OdometerActionScreen::OdometerActionScreen(uint8 odometer)
 {
@@ -17,15 +10,29 @@ OdometerActionScreen::OdometerActionScreen(uint8 odometer)
 
 const char * OdometerActionScreen::getItemText(uint8 idx) const
 {
-	return actions[idx];	
+	if(idx == 1)
+		return "Reset";
+
+	// Assume idx=0 here, so actual text depends on odometer state	
+	GPSOdometer odometer = gpsDataModel.getOdometer(odometerID);
+	
+	// Active odometer can only be paused
+	if(odometer.isActive())
+		return "Pause";
+		
+	// Inactive is either already paused (and can be resumed) or even not started
+	if(odometer.getTotalTime() != 0)
+		return "Resume";
+		
+	return "Start";
 }
 
 uint8 OdometerActionScreen::getItemsCount() const
 {
-	return 4;	
+	return 2;
 }
 
 void OdometerActionScreen::applySelection(uint8 idx)
 {
-	showMessageBox(actions[idx]);
+	showMessageBox("Action");
 }
