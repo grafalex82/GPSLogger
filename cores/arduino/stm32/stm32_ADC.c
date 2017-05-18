7a86f2d9b25af02f326bda0f314ec4dc348fbc4d
grafalex82 <grafalex@univ.kiev.ua>
2017-05-18 16:08:34 +0300

    Squashed 'Libs/STM32generic/' content from commit 4cfd4db
    git-subtree-dir: Libs/STM32generic
    git-subtree-split: 4cfd4db1e6c04ca53a5d6a59d97a7571a77188e5

---
+++ cores/arduino/stm32/stm32_ADC.c
@@ -0,0 +1,126 @@
+/*
+  Copyright (c) 2017 Daniel Fekete
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+/**
+ * Implementation of Arduino methods:
+ * analogRead(): https://www.arduino.cc/en/Reference/AnalogRead
+ *
+ * TODO actually create stm32_analog_in_XX.c files for the different variants. This is getting too compicated
+ *
+ * TODO improve performance by saving if the pin was configured for analog, and do not configure again.
+ *   also deconfigure from pinMode in this case
+ * TODO implement analogReadResolution
+ * TODO F1 has no resolution, map to arduino default 10 bits
+ * TODO check if ClockPrescaler can always be set to ADC_CLOCK_SYNC_PCLK_DIV2
+ */
+
+#include "stm32_gpio_af.h"
+
+void stm32_adc_init(ADC_HandleTypeDef *handle);
+
+#ifdef ADC_CLOCK_SYNC_PCLK_DIV2
+#define ADC_CLOCK_DIV ADC_CLOCK_SYNC_PCLK_DIV2
+#elif defined(ADC_CLOCK_ASYNC_DIV1)
+#define ADC_CLOCK_DIV ADC_CLOCK_ASYNC_DIV1
+#elif defined(ADC_CLOCKPRESCALER_PCLK_DIV2)
+#define ADC_CLOCK_DIV ADC_CLOCKPRESCALER_PCLK_DIV2
+#else
+#error "Unknown clock"
+#endif
+
+
+int analogRead(uint8_t pin) {
+    static ADC_HandleTypeDef handle = {};
+
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_InitStruct.Pin = variant_pin_list[pin].pin_mask;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(variant_pin_list[pin].port, &GPIO_InitStruct);
+
+    if (handle.Instance == NULL) {
+        #ifdef __HAL_RCC_ADC1_CLK_ENABLE
+        __HAL_RCC_ADC1_CLK_ENABLE();
+        #endif
+        #ifdef __HAL_RCC_ADC_CLK_ENABLE
+        __HAL_RCC_ADC_CLK_ENABLE();
+        #endif
+        
+        handle.Instance = ADC1;
+        handle.Init.ScanConvMode = DISABLE;
+        handle.Init.ContinuousConvMode = ENABLE;
+        handle.Init.DiscontinuousConvMode = DISABLE;
+        handle.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+
+        #ifdef STM32L0
+            handle.Init.SamplingTime = ADC_SAMPLETIME_13CYCLES_5;
+        #endif
+
+        #if !defined(STM32L0) && !defined(STM32F0)
+            handle.Init.NbrOfConversion = 1;
+        #endif
+
+        #ifdef ADC_EOC_SINGLE_CONV
+            handle.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+        #endif
+
+        #ifdef STM32F1
+            handle.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+        #else
+            handle.Init.ClockPrescaler = ADC_CLOCK_DIV;
+            handle.Init.Resolution = ADC_RESOLUTION_12B;
+        #endif
+
+        HAL_ADC_Init(&handle);
+
+    }
+
+    ADC_ChannelConfTypeDef sConfig;
+    sConfig.Channel = stm32ADC1GetChannel(variant_pin_list[pin].port, variant_pin_list[pin].pin_mask);
+    sConfig.Rank = 1;
+
+    #if STM32L0
+        //in handle
+    #elif defined(ADC_SAMPLETIME_15CYCLES)
+        sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES;
+    #elif defined(ADC_SAMPLETIME_13CYCLES_5)
+        sConfig.SamplingTime = ADC_SAMPLETIME_13CYCLES_5;
+    #elif defined(ADC_SAMPLETIME_19CYCLES_5)
+        sConfig.SamplingTime = ADC_SAMPLETIME_19CYCLES_5;
+    #elif defined(ADC_SAMPLETIME_16CYCLES)
+        sConfig.SamplingTime = ADC_SAMPLETIME_16CYCLES;
+    #elif defined(ADC_SAMPLETIME_12CYCLES_5)
+        sConfig.SamplingTime = ADC_SAMPLETIME_12CYCLES_5;
+    #else
+        #error "unknown sampleing time"
+    #endif
+
+    HAL_ADC_ConfigChannel(&handle, &sConfig);
+
+    HAL_ADC_Start(&handle);
+
+    if (HAL_ADC_PollForConversion(&handle, 1000) != HAL_OK) {
+            return 0;
+    }
+
+    return HAL_ADC_GetValue(&handle) >> 2;
+}
---
+++ cores/arduino/stm32/stm32_gpio.h
@@ -0,0 +1,98 @@
+/*
+  Copyright (c) 2017 Daniel Fekete
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#ifndef STM32_GPIO_H
+#define STM32_GPIO_H
+
+#include "stm32_def.h"
+
+#include "variant.h"
+
+#define INPUT 0x0
+#define OUTPUT 0x1
+#define INPUT_PULLUP 0x2
+#define INPUT_PULLDOWN 0x3
+
+#ifndef GPIO_SPEED_FREQ_VERY_HIGH
+    #define GPIO_SPEED_FREQ_VERY_HIGH GPIO_SPEED_FREQ_HIGH
+#endif
+
+#define RwReg uint32_t
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+typedef struct {
+    GPIO_TypeDef *port;
+    uint32_t pin_mask;
+} stm32_port_pin_type;
+
+typedef uint32_t (*stm32_clock_freq_func)();
+
+typedef struct {
+    void *instance;
+    stm32_clock_freq_func clock_freq_func;
+} stm32_clock_freq_list_type;
+
+extern const stm32_port_pin_type variant_pin_list[NUM_PINS];
+
+/**
+ * Start clock for the fedined port
+ */
+void stm32GpioClockEnable(GPIO_TypeDef *port);
+
+/**
+ * If PWM is used at least once, this method is set to the PWM disable function in stm32_PWM.c
+ */
+typedef void (*stm32_pwm_disable_callback_func)(GPIO_TypeDef *port, uint32_t pin);
+extern stm32_pwm_disable_callback_func stm32_pwm_disable_callback;
+
+inline void digitalWrite(uint8_t pin, uint8_t value) {
+    if (pin >= sizeof(variant_pin_list) / sizeof(variant_pin_list[0])) {
+        return;
+    }
+    
+    stm32_port_pin_type port_pin = variant_pin_list[pin];
+    
+    HAL_GPIO_WritePin(port_pin.port, port_pin.pin_mask, value ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    
+}
+
+
+inline int digitalRead(uint8_t pin) {
+    if (pin >= sizeof(variant_pin_list) / sizeof(variant_pin_list[0])) {
+        return 0;
+    }
+    
+    stm32_port_pin_type port_pin = variant_pin_list[pin];
+    
+    return HAL_GPIO_ReadPin(port_pin.port, port_pin.pin_mask);
+    
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
