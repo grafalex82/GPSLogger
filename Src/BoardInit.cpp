7994340a055a6dd2b63a63796e4b6b833ba98af9
grafalex82 <grafalex@univ.kiev.ua>
2017-05-24 18:08:12 +0300

    Migrated to own copy of FreeRTOS 9.0.0

--- Src/BoardInit.cpp
+++ Src/BoardInit.cpp
@@ -7,6 +7,9 @@
 
 #include "BoardInit.h"
 
+#include "FreeRTOS.h"
+#include "task.h"
+
 // Libc requires _init() to perform some initialization before global constructors run.
 // I would love if this symbol is defined as weak in newlib-nano (libc), but it is not.
 extern "C" void _init(void)  {}
@@ -14,42 +17,42 @@ extern "C" void _init(void)  {}
 // Set up board clocks
 void SystemClock_Config(void)
 {
-    // Set up external oscillator to 72 MHz
-    RCC_OscInitTypeDef RCC_OscInitStruct;
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-    RCC_OscInitStruct.LSEState = RCC_LSE_OFF;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSICalibrationValue = 16;
-    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+	// Set up external oscillator to 72 MHz
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.LSEState = RCC_LSE_OFF;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = 16;
+	RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+	HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-    // Set up periperal clocking
-    RCC_ClkInitTypeDef RCC_ClkInitStruct;
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                                  |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
+	// Set up periperal clocking
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+								  |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
 /*
-    // Set up USB clock
-    RCC_PeriphCLKInitTypeDef PeriphClkInit;
-    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
-    PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
-    HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
+	// Set up USB clock
+	RCC_PeriphCLKInitTypeDef PeriphClkInit;
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+	PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
+	HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
 */
-    // Set up SysTTick to 1 ms
-    // TODO: Do we really need this? SysTick is initialized multiple times in HAL
-    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
-    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+	// Set up SysTTick to 1 ms
+	// TODO: Do we really need this? SysTick is initialized multiple times in HAL
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
-    // SysTick_IRQn interrupt configuration
-    HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+	// SysTick_IRQn interrupt configuration
+	HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
 
 
@@ -62,9 +65,38 @@ extern "C" void SysTick_Handler(void)
 
 void InitBoard()
 {
-    // Initialize board and HAL
-    HAL_Init();
-    SystemClock_Config();
+	// Initialize board and HAL
+	HAL_Init();
+	SystemClock_Config();
 
-    HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+}
+
+extern "C"
+void vApplicationStackOverflowHook(xTaskHandle *pxTask,
+								   signed char *pcTaskName)
+{
+	/* This function will get called if a task overflows its stack.
+	 * If the parameters are corrupt then inspect pxCurrentTCB to find
+	 * which was the offending task. */
+
+	(void) pxTask;
+	(void) pcTaskName;
+
+	while (1)
+		;
+}
+
+// TODO: temporary living here. Will be removed when IdleThread is integrated back
+extern "C" void vApplicationIdleHook( void )
+{
+}
+
+extern "C" void vApplicationMallocFailedHook( void )
+{
+	/* vApplicationMallocFailedHook() will only be called if
+	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+	function that will get called if a call to pvPortMalloc() fails.*/
+	//taskDISABLE_INTERRUPTS();
+	for( ;; );
 }
--- Src/GPSLogger.cpp
+++ Src/GPSLogger.cpp
@@ -1,5 +1,8 @@
 #include <stm32f1xx_hal.h>
-//#include <MapleFreeRTOS821.h>
+
+// FreeRTOS
+#include <FreeRTOS.h>
+#include "task.h"
 
 //#include "Screens/ScreenManager.h"
 //#include "Buttons.h"
@@ -8,20 +11,19 @@
 #include "BoardInit.h"
 #include "LEDDriver.h"
 
-//#include "Arduino.h"
 
-/*
 void vLEDFlashTask(void *pvParameters) 
 {
-	for (;;) 
+	LEDDriver led;
+
+	for (;;)
 	{
 		vTaskDelay(2000);
-		digitalWrite(PC13, LOW);
+		led.turnOn();
 		vTaskDelay(100);
-		digitalWrite(PC13, HIGH);
+		led.turnOff();
 	}
 }
-*/
 
 /*
 // the setup function runs once when you press reset or power the board
@@ -62,20 +64,23 @@ int main(void)
 {
 	InitBoard();
 
-	LEDDriver led;
+	//SerialUSB.begin(115200);
 
-	// Main loop
-	for (;;)
-	{
-		led.toggle();
-		HAL_Delay(1500);
-		led.toggle();
-		HAL_Delay(200);
-		led.toggle();
-		HAL_Delay(200);
-		led.toggle();
-		HAL_Delay(200);
-	}
+	//initDisplay();
+	//initButtons();
+	//initScreens();
+	//initGPS();
 
+	// Set up threads
+	// TODO: Consider encapsulating init and task functions into a class(es)
+	xTaskCreate(vLEDFlashTask, "LED Task",	configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);
+	//xTaskCreate(vDisplayTask, "Display Task", 1024, NULL, tskIDLE_PRIORITY + 2, NULL);
+	//xTaskCreate(vButtonsTask, "Buttons Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);
+	//xTaskCreate(vGPSTask, "GPS Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 3, NULL);
+
+	// Run scheduler and all the threads
+	vTaskStartScheduler();
+
+	// Never going to be here
 	return 0;
 }
