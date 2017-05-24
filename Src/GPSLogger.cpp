2a2aaab73244505ab6bf7984548a21682a7d651a
grafalex82 <grafalex@univ.kiev.ua>
2017-05-24 13:37:53 +0300

    Board initialization moved to a separate file

--- Src/GPSLogger.cpp
+++ Src/GPSLogger.cpp
@@ -1,15 +1,14 @@
 #include <stm32f1xx_hal.h>
-#include <stm32f1xx_hal_gpio.h>
 #include <stm32f1xx_hal_rcc.h>
-#include <stm32f1xx_hal_rcc_ex.h>
-#include <stm32f1xx_hal_flash.h>
-#include <stm32f1xx_hal_cortex.h>
+#include <stm32f1xx_hal_gpio.h>
 
 //#include <MapleFreeRTOS821.h>
 
 //#include "Screens/ScreenManager.h"
 //#include "Buttons.h"
 //#include "GPSThread.h"
+
+#include "BoardInit.h"
 
 //#include "Arduino.h"
 
@@ -62,220 +61,12 @@ void loop()
 }
 */
 
-// Libc requires _init() to perform some initialization before global constructors run.
-// I would love if this symbol is defined as weak in newlib-nano (libc), but it is not.
-extern "C" void _init(void)  {}
-
-void SystemClock_Config(void)
-{	
-	// Set up external oscillator to 72 MHz
-	RCC_OscInitTypeDef RCC_OscInitStruct;
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.LSEState = RCC_LSE_OFF;
-	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	RCC_OscInitStruct.HSICalibrationValue = 16;
-	RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-	HAL_RCC_OscConfig(&RCC_OscInitStruct);
-
-	// Set up periperal clocking
-	RCC_ClkInitTypeDef RCC_ClkInitStruct;
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-	                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
-/*
-	// Set up USB clock
-	RCC_PeriphCLKInitTypeDef PeriphClkInit;
-	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB; 
-	PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5; 
-	HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
-*/
-	// Set up SysTTick to 1 ms
-	// TODO: Do we really need this? SysTick is initialized multiple times in HAL
-	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
-	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-
-	// SysTick_IRQn interrupt configuration
-	HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
-}
-
-/**
-* @brief This function handles System tick timer.
-*/
-extern "C"
-{
-
-
-void SysTick_Handler(void)
-{
-  /* USER CODE BEGIN SysTick_IRQn 0 */
-
-  /* USER CODE END SysTick_IRQn 0 */
-  HAL_IncTick();
-  HAL_SYSTICK_IRQHandler();
-  /* USER CODE BEGIN SysTick_IRQn 1 */
-
-  /* USER CODE END SysTick_IRQn 1 */
-}
-
-void HAL_MspInit(void)
-{
-	__HAL_RCC_AFIO_CLK_ENABLE();
-
-	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
-
-	/* System interrupt init*/
-	/* MemoryManagement_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(MemoryManagement_IRQn, 0, 0);
-	/* BusFault_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(BusFault_IRQn, 0, 0);
-	/* UsageFault_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(UsageFault_IRQn, 0, 0);
-	/* SVCall_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(SVCall_IRQn, 0, 0);
-	/* DebugMonitor_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(DebugMonitor_IRQn, 0, 0);
-	/* PendSV_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(PendSV_IRQn, 0, 0);
-	/* SysTick_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
-
-	/**DISABLE: JTAG-DP Disabled and SW-DP Disabled
-	*/
-	__HAL_AFIO_REMAP_SWJ_DISABLE();
-}
-
-void NMI_Handler(void)
-{
-  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
-
-  /* USER CODE END NonMaskableInt_IRQn 0 */
-  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
-
-  /* USER CODE END NonMaskableInt_IRQn 1 */
-}
-
-/**
-* @brief This function handles Hard fault interrupt.
-*/
-void HardFault_Handler(void)
-{
-  /* USER CODE BEGIN HardFault_IRQn 0 */
-
-  /* USER CODE END HardFault_IRQn 0 */
-  while (1)
-  {
-  }
-  /* USER CODE BEGIN HardFault_IRQn 1 */
-
-  /* USER CODE END HardFault_IRQn 1 */
-}
-
-/**
-* @brief This function handles Memory management fault.
-*/
-void MemManage_Handler(void)
-{
-  /* USER CODE BEGIN MemoryManagement_IRQn 0 */
-
-  /* USER CODE END MemoryManagement_IRQn 0 */
-  while (1)
-  {
-  }
-  /* USER CODE BEGIN MemoryManagement_IRQn 1 */
-
-  /* USER CODE END MemoryManagement_IRQn 1 */
-}
-
-/**
-* @brief This function handles Prefetch fault, memory access fault.
-*/
-void BusFault_Handler(void)
-{
-  /* USER CODE BEGIN BusFault_IRQn 0 */
-
-  /* USER CODE END BusFault_IRQn 0 */
-  while (1)
-  {
-  }
-  /* USER CODE BEGIN BusFault_IRQn 1 */
-
-  /* USER CODE END BusFault_IRQn 1 */
-}
-
-/**
-* @brief This function handles Undefined instruction or illegal state.
-*/
-void UsageFault_Handler(void)
-{
-  /* USER CODE BEGIN UsageFault_IRQn 0 */
-
-  /* USER CODE END UsageFault_IRQn 0 */
-  while (1)
-  {
-  }
-  /* USER CODE BEGIN UsageFault_IRQn 1 */
-
-  /* USER CODE END UsageFault_IRQn 1 */
-}
-
-/**
-* @brief This function handles System service call via SWI instruction.
-*/
-void SVC_Handler(void)
-{
-  /* USER CODE BEGIN SVCall_IRQn 0 */
-
-  /* USER CODE END SVCall_IRQn 0 */
-  /* USER CODE BEGIN SVCall_IRQn 1 */
-
-  /* USER CODE END SVCall_IRQn 1 */
-}
-
-/**
-* @brief This function handles Debug monitor.
-*/
-void DebugMon_Handler(void)
-{
-  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
-
-  /* USER CODE END DebugMonitor_IRQn 0 */
-  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
-
-  /* USER CODE END DebugMonitor_IRQn 1 */
-}
-
-/**
-* @brief This function handles Pendable request for system service.
-*/
-void PendSV_Handler(void)
-{
-  /* USER CODE BEGIN PendSV_IRQn 0 */
-
-  /* USER CODE END PendSV_IRQn 0 */
-  /* USER CODE BEGIN PendSV_IRQn 1 */
-
-  /* USER CODE END PendSV_IRQn 1 */
-}
-
-}
 
 
 
 int main(void)
 {
-	// Initialize board and HAL
-        HAL_Init();
-	//NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x2000);
-	SystemClock_Config();
-	//__HAL_RCC_AFIO_CLK_ENABLE();
+    InitBoard();
 	
 	// enable clock to GPIOC
 	__HAL_RCC_GPIOC_CLK_ENABLE();
@@ -293,11 +84,13 @@ int main(void)
 	// Main loop
 	for (;;) 
 	{
-		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-		HAL_Delay(1500);
-		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-		HAL_Delay(500);
-	}
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        HAL_Delay(1500);
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        HAL_Delay(200);
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        HAL_Delay(200);
+    }
 
 	return 0;
 }
