94ce1da66cca725af2a7ab3e9eae70d6b74e5aa9
grafalex82 <grafalex@univ.kiev.ua>
2017-05-26 15:20:07 +0300

    Blinking moved to a separate file (LEDThread)

--- Src/GPSLogger.cpp
+++ Src/GPSLogger.cpp
@@ -1,4 +1,3 @@
-#include <stm32f1xx_hal.h>
 
 // FreeRTOS
 #include <FreeRTOS.h>
@@ -9,55 +8,7 @@
 //#include "GPSThread.h"
 
 #include "BoardInit.h"
-#include "LEDDriver.h"
-
-
-void vLEDFlashTask(void *pvParameters) 
-{
-	LEDDriver led;
-
-	for (;;)
-	{
-		vTaskDelay(2000);
-		led.turnOn();
-		vTaskDelay(100);
-		led.turnOff();
-	}
-}
-
-/*
-// the setup function runs once when you press reset or power the board
-void setup()
-{
-
-	pinMode(PC13, OUTPUT);
-	
-	SerialUSB.begin(115200);
-
-	initDisplay();
-	initButtons();
-	initScreens();
-	initGPS();
-	
-	// Set up threads
-	// TODO: Consider encapsulating init and task functions into a class(es)
-	xTaskCreate(vLEDFlashTask, "LED Task",	configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);
-	xTaskCreate(vDisplayTask, "Display Task", 1024, NULL, tskIDLE_PRIORITY + 2, NULL);
-	xTaskCreate(vButtonsTask, "Buttons Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);
-	xTaskCreate(vGPSTask, "GPS Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 3, NULL);
-
-	// Run scheduler and all the threads
-	vTaskStartScheduler();
-
-}
-
-// Arduino boilerplate function. Do not write any code here, it would not execute.
-void loop()
-{
-}
-*/
-
-
+#include "LEDThread.h"
 
 
 int main(void)
@@ -73,7 +24,7 @@ int main(void)
 
 	// Set up threads
 	// TODO: Consider encapsulating init and task functions into a class(es)
-	xTaskCreate(vLEDFlashTask, "LED Task",	configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);
+	xTaskCreate(vLEDThread, "LED Thread",	configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);
 	//xTaskCreate(vDisplayTask, "Display Task", 1024, NULL, tskIDLE_PRIORITY + 2, NULL);
 	//xTaskCreate(vButtonsTask, "Buttons Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);
 	//xTaskCreate(vGPSTask, "GPS Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 3, NULL);
---
+++ Src/LEDThread.cpp
@@ -0,0 +1,66 @@
+#include <stm32f1xx_hal.h>
+#include <stm32f1xx_hal_rcc.h>
+#include <stm32f1xx_hal_gpio.h>
+
+#include "LEDThread.h"
+
+#include <FreeRTOS.h>
+#include <task.h>
+
+#define LED_PORT GPIOC
+const uint16_t LED_PIN = GPIO_PIN_13;
+
+// Class to encapsulate working with onboard LED(s)
+//
+// Note: this class initializes corresponding pins in the constructor.
+//       May not be working properly if objects of this class are created as global variables
+class LEDDriver
+{
+public:
+	LEDDriver()
+	{
+		// enable clock to GPIOC
+		__HAL_RCC_GPIOC_CLK_ENABLE();
+		__HAL_RCC_GPIOD_CLK_ENABLE();
+
+		// Turn off the LED by default
+		HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
+
+		// Initialize PC13 pin
+		GPIO_InitTypeDef ledPinInit;
+		ledPinInit.Pin = LED_PIN;
+		ledPinInit.Mode = GPIO_MODE_OUTPUT_PP;
+		ledPinInit.Speed = GPIO_SPEED_FREQ_LOW;
+		HAL_GPIO_Init(LED_PORT, &ledPinInit);
+	}
+
+	void turnOn()
+	{
+		HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_RESET);
+	}
+
+	void turnOff()
+	{
+		HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
+	}
+
+	void toggle()
+	{
+		HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
+	}
+};
+
+
+void vLEDThread(void *pvParameters)
+{
+	LEDDriver led;
+
+	// Just blink once in 2 seconds
+	for (;;)
+	{
+		vTaskDelay(2000);
+		led.turnOn();
+		vTaskDelay(100);
+		led.turnOff();
+	}
+}
