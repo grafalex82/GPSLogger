3230829ea6d6d3dd6afa47ea0eab1e13d0d30983
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2017-05-29 19:35:57 +0300

    using libmaple's VID/PID to be compatible with maple bootloader

--- Libs/USB/Core/usbd_desc.c
+++ Libs/USB/Core/usbd_desc.c
@@ -71,14 +71,14 @@
 /** @defgroup USBD_DESC_Private_Defines
   * @{
   */ 
-#define USBD_VID     1155
-#define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22336
-#define USBD_PRODUCT_STRING_FS     "STM32 Virtual ComPort"
-#define USBD_SERIALNUMBER_STRING_FS     "00000000001A"
-#define USBD_CONFIGURATION_STRING_FS     "CDC Config"
-#define USBD_INTERFACE_STRING_FS     "CDC Interface"
+#define USBD_VID						0x1EAF
+#define USBD_LANGID_STRING				0x409
+#define USBD_MANUFACTURER_STRING		"Leaf Labs"
+#define USBD_PID_FS						0x0004
+#define USBD_PRODUCT_STRING_FS			"Maple"
+#define USBD_SERIALNUMBER_STRING_FS		"00000000001A"
+#define USBD_CONFIGURATION_STRING_FS    "CDC Config"
+#define USBD_INTERFACE_STRING_FS		"CDC Interface"
 
 /* USER CODE BEGIN 0 */
 
