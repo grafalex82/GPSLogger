2bd18363ac42990554ad429f2b3a6a307272eba2
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2017-05-31 21:38:15 +0300

    using USB initialization from stm32generic

--- Src/GPSLogger.cpp
+++ Src/GPSLogger.cpp
@@ -3,65 +3,14 @@
 #include <FreeRTOS.h>
 #include "task.h"
 
+#include "USBDevice.h"
+
 //#include "Screens/ScreenManager.h"
 //#include "GPSThread.h"
 
 #include "BoardInit.h"
 #include "LEDThread.h"
 #include "ButtonsThread.h"
-
-#include "usbd_core.h"
-#include "usbd_desc.h"
-#include "usbd_cdc.h"
-#include "usbd_cdc_if.h"
-
-
-
-extern "C" void _Error_Handler(char * file, int line)
-{
-
-}
-
-USBD_HandleTypeDef hUsbDeviceFS;
-
-void reenumerate()
-{
-
-	// Initialize PA12 pin
-	GPIO_InitTypeDef pinInit;
-	pinInit.Pin = GPIO_PIN_12;
-	pinInit.Mode = GPIO_MODE_OUTPUT_PP;
-	pinInit.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOA, &pinInit);
-
-	// Let host know to enumerate USB devices on the bus
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET);
-	for(unsigned int i=0; i<512; i++) {};
-
-	// Restore pin mode
-	pinInit.Mode = GPIO_MODE_INPUT;
-	pinInit.Pull = GPIO_NOPULL;
-	HAL_GPIO_Init(GPIOA, &pinInit);
-	for(unsigned int i=0; i<512; i++) {};
-}
-
-void initUSB()
-{
-	reenumerate();
-
-	USBD_Init(&hUsbDeviceFS, &CDC_Desc, DEVICE_FS);
-	USBD_RegisterClass(&hUsbDeviceFS, &USBD_CDC);
-	USBD_CDC_RegisterInterface(&hUsbDeviceFS, &USBD_Interface_fops_FS);
-	USBD_Start(&hUsbDeviceFS);
-
-}
-
-extern PCD_HandleTypeDef hpcd_USB_FS;
-
-extern "C" void USB_LP_CAN1_RX0_IRQHandler(void) {
-  HAL_PCD_IRQHandler(&hpcd_USB_FS);
-}
-
 
 
 int main(void)
@@ -75,8 +24,8 @@ int main(void)
 	//initScreens();
 	//initGPS();
 
-	initUSB();
-
+	USBDeviceFS.reenumerate();
+	USBDeviceFS.beginCDC();
 
 	// Set up threads
 	// TODO: Consider encapsulating init and task functions into a class(es)
