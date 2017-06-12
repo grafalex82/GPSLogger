98d697c867fd854f8a68c9d41764a752a38a863d
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2017-06-12 22:41:42 +0300

    Allocating more memory to account SD queue

--- Libs/STM32generic/libraries/FreeRTOS/src/FreeRTOSConfig.h
+++ Libs/STM32generic/libraries/FreeRTOS/src/FreeRTOSConfig.h
@@ -98,7 +98,7 @@
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 4 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 128 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 8 * 1024 ) )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 10 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 10 )
 #define configUSE_TRACE_FACILITY		0
 #define configUSE_16_BIT_TICKS			0
