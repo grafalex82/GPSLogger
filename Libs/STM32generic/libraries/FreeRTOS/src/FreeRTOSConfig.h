461eb6a3847eac103a66d7d7b37fce8a10086cf1
grafalex82 <grafalex@univ.kiev.ua>
2017-06-15 15:43:31 +0300

    Fixing long initialization hang (incorrect processing of SysTick event when Scheduler is not started)

--- Libs/STM32generic/libraries/FreeRTOS/src/FreeRTOSConfig.h
+++ Libs/STM32generic/libraries/FreeRTOS/src/FreeRTOSConfig.h
@@ -99,7 +99,7 @@
 #define configMAX_PRIORITIES			( 4 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 128 )
 #define configTOTAL_HEAP_SIZE			( ( size_t ) ( 8 * 1024 ) )
-#define configMAX_TASK_NAME_LEN			( 10 )
+#define configMAX_TASK_NAME_LEN			( 16 )
 #define configUSE_TRACE_FACILITY		0
 #define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			1
@@ -111,6 +111,7 @@
 #define configUSE_APPLICATION_TASK_TAG	0
 #define configUSE_COUNTING_SEMAPHORES	1
 #define configGENERATE_RUN_TIME_STATS	0
+#define configSUPPORT_STATIC_ALLOCATION	0
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES 		0
@@ -124,14 +125,15 @@
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
-#define INCLUDE_vTaskPrioritySet		0
-#define INCLUDE_uxTaskPriorityGet		0
-#define INCLUDE_vTaskDelete			0
+#define INCLUDE_vTaskPrioritySet			0
+#define INCLUDE_uxTaskPriorityGet			0
+#define INCLUDE_vTaskDelete					0
 #define INCLUDE_vTaskCleanUpResources		0
-#define INCLUDE_vTaskSuspend			0
-#define INCLUDE_vTaskDelayUntil			1
-#define INCLUDE_vTaskDelay			1
+#define INCLUDE_vTaskSuspend				0
+#define INCLUDE_vTaskDelayUntil				1
+#define INCLUDE_vTaskDelay					1
 #define INCLUDE_uxTaskGetStackHighWaterMark	0
+#define INCLUDE_xTaskGetSchedulerState		1
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
