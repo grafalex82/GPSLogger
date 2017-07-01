d27240debb21e20eaa6813df713d2e12e9acf7d2
grafalex82 <grafalex@univ.kiev.ua>
2017-07-01 21:39:47 +0300

    Removing extra USB Transmit buffer

--- Libs/USB/CDC/usbd_cdc_if.c
+++ Libs/USB/CDC/usbd_cdc_if.c
@@ -98,10 +98,11 @@
 /* Create buffer for reception and transmission           */
 /* It's up to user to redefine and/or remove those define */
 /* Received Data over USB are stored in this buffer       */
-uint8_t UserRxBufferFS[APP_RX_DATA_SIZE];
+uint8_t UserRxBufferFS[32]; // Not really used. Reserving just a little room for Magic String
 
 /* Send Data over USB CDC are stored in this buffer       */
-uint8_t UserTxBufferFS[APP_TX_DATA_SIZE];
+//uint8_t UserTxBufferFS[APP_TX_DATA_SIZE];
+extern uint8_t * usbTxBuffer;
 
 /* USER CODE BEGIN PRIVATE_VARIABLES */
 
@@ -158,7 +159,7 @@ static int8_t CDC_Init_FS(void)
 { 
   /* USER CODE BEGIN 3 */ 
   /* Set Application Buffers */
-  USBD_CDC_SetTxBuffer(&hUsbDeviceFS, UserTxBufferFS, 0);
+  USBD_CDC_SetTxBuffer(&hUsbDeviceFS, usbTxBuffer, 0);
   USBD_CDC_SetRxBuffer(&hUsbDeviceFS, UserRxBufferFS);
   return (USBD_OK);
   /* USER CODE END 3 */ 
