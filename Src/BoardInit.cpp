918173b1aa9ccfca289226995c4e6ad107de2fc6
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2019-06-08 20:58:55 +0300

    Finally switch from STM32GENERIC to MiniArduino

--- Src/BoardInit.cpp
+++ Src/BoardInit.cpp
@@ -64,25 +64,39 @@ void InitBoard()
 }
 
 extern "C"
-void vApplicationStackOverflowHook(xTaskHandle *pxTask,
-								   signed char *pcTaskName)
 {
-	/* This function will get called if a task overflows its stack.
-	 * If the parameters are corrupt then inspect pxCurrentTCB to find
-	 * which was the offending task. */
+	void SysTick_Handler(void) {
+	  HAL_IncTick();
+	  HAL_SYSTICK_IRQHandler();
+	}
 
-	(void) pxTask;
-	(void) pcTaskName;
+	extern "C"
+	void Error_Handler()
+	{
 
-	while (1)
-		;
-}
+	}
 
-extern "C" void vApplicationMallocFailedHook( void )
-{
-	/* vApplicationMallocFailedHook() will only be called if
-	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
-	function that will get called if a call to pvPortMalloc() fails.*/
-	//taskDISABLE_INTERRUPTS();
-	for( ;; );
+	extern "C"
+	void vApplicationStackOverflowHook(xTaskHandle *pxTask,
+									   signed char *pcTaskName)
+	{
+		/* This function will get called if a task overflows its stack.
+		 * If the parameters are corrupt then inspect pxCurrentTCB to find
+		 * which was the offending task. */
+
+		(void) pxTask;
+		(void) pcTaskName;
+
+		while (1)
+			;
+	}
+
+	extern "C" void vApplicationMallocFailedHook( void )
+	{
+		/* vApplicationMallocFailedHook() will only be called if
+		configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+		function that will get called if a call to pvPortMalloc() fails.*/
+		//taskDISABLE_INTERRUPTS();
+		for( ;; );
+	}
 }
